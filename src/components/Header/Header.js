f8a1d25ab59944c9e1d6255155a13f7ebf22e564
Spencer Smith <stix1919@hotmail.com>
2018-09-06 12:03:16 -0500

    Header styling near done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,35 @@ import axios from 'axios'
 import './Header.css';
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            menu: false,
+            links: ['Home', 'About', 'Contact']
+        }
+        this.openMenu = this.openMenu.bind(this)
+    }
 
+    openMenu() {
+        this.setState({menu: !this.state.menu})
+    }
 
   render() {
+    let navLinks = this.state.links.map(link => {
+        return (
+            <h4 className='nav-link'>{link}</h4>
+        )
+    })
     return (
       <div className='App-header'>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <img className='hamBar' src='https://cdn4.iconfinder.com/data/icons/tupix-1/30/list-512.png' alt='MenuBar'/>
+            <img src={logo} className="App-logo" alt="logo" />
+            <div>
+                <h1 className="App-title">A website</h1>
+                <img className='hamBar' src='https://cdn4.iconfinder.com/data/icons/tupix-1/30/list-512.png' alt='MenuBar'onClick={() => this.openMenu()}/>
+                <div className={this.state.menu ? 'dropdownMenu' : 'menu-Closed'}>
+                    {navLinks}
+                </div>
+            </div>
       </div>
     );
   }
