77dd54bdb9a7b6efbc5c64e55778f414c4e275db
Spencer Smith <stix1919@hotmail.com>
2018-09-07 11:32:53 -0500

    even more setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { json } = require('body-parser')
+const session = require('express-session')
+const massive = require('massive')
 
 const port = 3001
 
 const app = express()
+
 
 app.use(json())
 
