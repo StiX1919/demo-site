8239ac3135bdf76d0c9051146ca36d8bed117ab8
Spencer Smith <stix1919@hotmail.com>
2018-10-20 21:07:33 -0500

    notes in router

--- src/routers/gamesRouter.js
+++ src/routers/gamesRouter.js
@@ -14,6 +14,7 @@ import HeroHub from '../components/Games/MegaRPG/components/HeroHub/HeroHub'
 import PixelArt from '../components/Games/PixelArt/PixelArt'
 
 // import Games from '../components/Games/Games'
+// might recombine routers
 
 export default (
         <Switch>
